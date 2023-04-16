8d5c9b8924fa7e35cad5139f981af2385148c1bf
Chris Walker <hello@chrisashwalker.com>
2023-04-16 21:29:51 +0100

    Remove old tests

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from app import main
-
-# see: http://flask.pocoo.org/docs/1.0/testing/
-@pytest.fixture
-def client():
-    main.app.config['TESTING'] = True
-    client = main.app.test_client()
-    yield client
--- tests/data/test_greeter.py
+++
@@ -1,4 +0,0 @@
-from data.greetings import say_hello_to
-
-def test_say_hello_appends_name():
-    assert say_hello_to("world") == "hello world"
